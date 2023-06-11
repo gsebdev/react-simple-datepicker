611637bcc08cd49c2eb6f55979bc89b503dd2ae2
Sebastien Gault <sgault.webdev@gmail.com>
2023-06-11 23:40:19 +0200

    changed build scripts, webpackconfig, babel config, resolved minor bug on scss

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,6 @@ module.exports = {
         exclude: /node_modules/, 
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-          },
         },
       },
       {
